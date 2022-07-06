983a0143bcea4e0b0ed3d719f16fa365720b8a87
idiotic-bird <64965130+idiotic-bird@users.noreply.github.com>
2022-07-06 21:30:21 +0800

    this is a test

---
+++ test0.py
@@ -0,0 +1,23 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import math
+
+def f(x0):
+    return x0 ** 2 - 1 - x0
+
+x = list(np.arange(-3, 3, 0.01))
+y = list(map(f, x))
+plt.plot(x,y)
+x_0 = 10000
+x_1 = 1.3
+result_analysis = []
+idx = 0
+while abs(x_0 - x_1) > 1e-10:
+    x_0 = x_1
+    x_1 = math.sqrt(x_0 + 1)
+    result_analysis.append(x_1)
+    if idx > 100:
+        break
+    idx += 1
+    print(idx)
+plt.plot(result_analysis)
